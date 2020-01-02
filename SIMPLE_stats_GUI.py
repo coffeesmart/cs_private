5159cb65b5414bb8b2578d95ea7c3611f9074aa4
coffeesmart <coffeesmart@naver.com>
2020-01-02 17:22:30 +0900

    practice for making GUI

--- SIMPLE_stats_GUI.py
+++ SIMPLE_stats_GUI.py
@@ -1,4 +1,33 @@
-import numpy as np
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QCoreApplication
 
-a = np.around(1,1)
-print(a)
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        btn = QPushButton('Quit', self)
+        btn.move(150, 350)
+        btn.resize(btn.sizeHint())
+        btn.clicked.connect(QCoreApplication.instance().quit)
+
+        self.setWindowTitle('My First Application')
+        self.setWindowIcon(QIcon('web.png'))
+        self.setGeometry(300, 300, 400, 400)
+        self.show()
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
+
+
